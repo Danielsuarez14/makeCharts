36400877c60f30c6ab940c76e880007bb4deed35
Daniel Suarez <danielsuarez2418@gmail.com>
2025-09-16 11:57:53 -0500

    Add: Sound when you click the card button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,24 @@
 import { useNavigate } from "react-router-dom";
+import useSound from "use-sound"
+import sound from '../../sounds/sound.wav'
 
 function Card(card) {
     let Title = card.title
     let Image = card.image
     let Id = card.id
     const navigate = useNavigate();
+    const [play] = useSound(sound)
+
     return (
         <div className="card" id={Title}>
             <h2>
                 {Title}
             </h2>
             <img src={Image} alt={Title} />
-            <button className="goButton" onClick={() => navigate(`${Id}Chart`)}>Let's Go</button>
+            <button className="goButton" onClick={() => {
+                navigate(`${Id}Chart`);
+                play()
+            }}>Let's Go</button>
         </div>
     )
 }
