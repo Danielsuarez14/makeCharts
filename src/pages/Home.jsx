a90f8d4eac486099fb347dba9fb395ae7e22267f
Daniel Suarez <danielsuarez2418@gmail.com>
2025-09-16 11:48:40 -0500

    Add animations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,20 +5,23 @@ function Home() {
         <div className="home">
             <div className="header">
                 <h1 translate="no" className="title">Make Charts</h1>
-                <p className="introduce">Don't worry about making charts, Make Charts is very intuitive and your charts will look great</p>
+                <div className="introduce">
+                    <p>Don't worry about making charts, Make Charts is very intuitive and your charts will look great</p>
+                </div>
+
                 <img src='./player.png' alt="graphics" />
             </div>
             <div className="selectText">
                 <h2>Select one and hands to work</h2>
             </div>
             <div className="chartOptions">
-                <Card id={'area'} title={'Area Chart'} image={'./area.png'}/>
-                <Card id={'bar'} title={'Bar Chart'} image={'./bar.png'}/>
-                <Card id={'pie'} title={'Doughnut and Pie Charts'} image={'./pie.png'}/>
-                <Card id={'line'} title={'Line Chart'} image={'./line.png'}/>
-                <Card id={'polar'} title={'Polar Area Chart'} image={'./polar.png'}/>
-                <Card id={'radar'} title={'Radar Chart'} image={'./radar.png'}/>
-                <Card id={'scatter'} title={'Scatter Chart'} image={'./scatter.png'}/>
+                <Card id={'area'} title={'Area Chart'} image={'./area.png'} />
+                <Card id={'bar'} title={'Bar Chart'} image={'./bar.png'} />
+                <Card id={'pie'} title={'Doughnut and Pie Charts'} image={'./pie.png'} />
+                <Card id={'line'} title={'Line Chart'} image={'./line.png'} />
+                <Card id={'polar'} title={'Polar Area Chart'} image={'./polar.png'} />
+                <Card id={'radar'} title={'Radar Chart'} image={'./radar.png'} />
+                <Card id={'scatter'} title={'Scatter Chart'} image={'./scatter.png'} />
             </div>
         </div>
     )
